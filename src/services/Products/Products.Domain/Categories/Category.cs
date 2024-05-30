de99733a067f93b657bfc6755cdb79fc91f4d067
Mohammad Noori <144573411+MohammadNuri@users.noreply.github.com>
2024-05-30 23:02:18 +0330

    init docker + Product/Category Entites

---
+++ src/services/Products/Products.Domain/Categories/Category.cs
@@ -0,0 +1,29 @@
+﻿using Products.Domain.Base;
+using Products.Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Domain.Categories
+{
+	public class Category : BaseEntity
+	{
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Permalink { get; set; }
+        public bool Active { get; set; }
+        public int Priority { get; set; }
+		public string BannerUrl { get; set; }
+		public string IconUrl { get; set; }
+		public string ThumbnailUrl { get; set; }
+
+		public ICollection<Product> Products { get; set; }
+
+		public int? ParentCategoryId { get; set; }
+		public Category ParentCategory { get; set; }
+
+	}
+}
---
+++ src/services/Products/Products.Domain/Products/Product.cs
@@ -0,0 +1,26 @@
+﻿using Products.Domain.Base;
+using Products.Domain.Categories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Domain.Products
+{
+	public class Product : BaseEntity
+	{
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+        public string Permalink { get; set; }
+        public string CoverImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public string Code { get; set; }
+
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
